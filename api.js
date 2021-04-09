5df43209ec1c3357b0d3175e9aa2fb3c04ff6d10
johirjohnny <johirjohnny96@gmail.com>
2021-04-09 12:08:43 +0600

    final commit

--- api.js
+++ api.js
@@ -3,13 +3,14 @@ const searchBtn = document.getElementById('searchBtn');
 const message = document.getElementById('message');
 const details = document.getElementById('details');
 
+
 searchBtn.addEventListener('click', function () {
     const keyword = document.getElementById('keyword').value;
     document.getElementById('keyword').value = '';
-    //allFoodsHere.innerHTML = '';
+    allFoodsHere.innerHTML = '';
 
     if (keyword === '') {
-        //const msg = document.getElementById('message');
+        const msg = document.getElementById('message');
         message.style.display = 'block';
         details.style.display = 'none';
     } else {
@@ -31,8 +32,7 @@ const displayMeals = meals => {
         const foodDiv = document.createElement('div');
         foodDiv.className = 'col-lg-3 col-md-4 pb-4';
         const foodInfo = `
-            <div onclick = "displayCountryDetail('${food.idMeal}')">
-           
+            <div onclick = "displayCountryDetail('${food.idMeal}')">           
             <img src="${food.strMealThumb}" alt="">
             <h2>${food.strMeal}</h2>
             </div>
@@ -41,29 +41,29 @@ const displayMeals = meals => {
         foodsDiv.appendChild(foodDiv);
     })
 
-    }
+}
 
 
-    
+
 const displayCountryDetail = name => {
-        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${name}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${name}`)
         .then(res => res.json())
         .then(data => {
             renderFoodInfo(data.meals[0]);
         })
+}
+const renderFoodInfo = food => {
+    allFoodsHere.innerHTML = '';
+    details.style.display = 'block';
+
+    const ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+        ingredients.push(`${food[`strIngredient${i}`]}`);
+
     }
-    const renderFoodInfo = food => {
-        allFoodsHere.innerHTML = '';
-        details.style.display = 'block';
 
-         const ingredients = [];
-         for (let i = 1; i <= 20; i++) {
-                 ingredients.push(`${food[`strIngredient${i}`]}`);
-             
-         }
-
-        const detailsDiv = document.getElementById('details');
-        detailsDiv.innerHTML = `
+    const detailsDiv = document.getElementById('details');
+    detailsDiv.innerHTML = `
             <div class="details-container">
                 <img src="${food.strMealThumb}" alt="">
                 <h4>${food.strMeal}</h4>
